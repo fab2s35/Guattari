9c4a9e8190563fe86896d0cc95043e0d421b86cb
fab2s35 <sosorto72@gmail.com>
2025-04-19 02:46:33 -0600

    avances en el header

--- Frontend/Guattari/src/components/Header.jsx
+++ Frontend/Guattari/src/components/Header.jsx
@@ -1,38 +1,40 @@
+import React from 'react';
+import './Header.css'; // 👈 Import del CSS
 
-function Header(){
-
+function Header() {
   return (
     <>
+   <div class="ads-text">
+  <span>Registrarse</span>
+  <span>/</span>
+  <span className='for-margin'>Iniciar Sesión</span>
+</div>
+
+<div>
+  <h3 className='text-center'>texto provisional</h3>
+</div>
+
       <nav className="navbar navbar-expand-lg navbar-light custom-navbar">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-            <a className="nav-item nav-link" href="#">Features</a>
-            <a className="nav-item nav-link" href="#">Pricing</a>
-            <a className="nav-item nav-link disabled" href="#">Disabled</a>
+            <a className="nav-item nav-link active" href="#">Inicio</a>
+            <a className="nav-item nav-link" href="#">Estancias</a>
+            <a className="nav-item nav-link" href="#">Sobre Nosotros</a>
+            <a className="nav-item nav-link" href="#">Contactanos</a>
           </div>
         </div>
       </nav>
-
-      <style jsx>{`
-        .custom-navbar {
-          background-color: #923922 !important;
-          width: 100%;
-        }
-        .navbar-light .navbar-nav .nav-link {
-          color: white; /* Cambia el color de los enlaces a blanco */
-        }
-        .navbar-light .navbar-nav .nav-link.active {
-          color: yellow; /* Color del enlace activo */
-        }
-        .navbar-light .navbar-toggler-icon {
-          background-color: white; /* Cambia el color del icono de la hamburguesa */
-        }
-      `}</style>
     </>
   );
 }
