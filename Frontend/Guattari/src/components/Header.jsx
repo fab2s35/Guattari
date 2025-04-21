107df3d03819428c4d349dc63e20b76a4a593c81
fab2s35 <sosorto72@gmail.com>
2025-04-20 22:18:19 -0600

    header finalizado

--- Frontend/Guattari/src/components/Header.jsx
+++ Frontend/Guattari/src/components/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Header.css'; // 👈 Import del CSS
+import logoGuattari from '../img/GUATTARI_logo-ver1.jpg';
+import IC_cuenta from '../img/user.png';
+import IC_cart from '../img/shopping-cart.png';
+
 
 function Header() {
   return (
@@ -10,11 +14,31 @@ function Header() {
   <span className='for-margin'>Iniciar Sesión</span>
 </div>
 
-<div>
-  <h3 className='text-center'>texto provisional</h3>
+
+
+<div className="container-fluid header-top d-flex align-items-center justify-content-between flex-wrap">
+  <div className="d-flex align-items-center logo-buscador">
+    <img src={logoGuattari} alt="Logo" className="logo" />
+    <div className="search-container">
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Buscar..."
+      />
+      <i className="fas fa-search search-icon"></i>
+    </div>
+  </div>
+
+  <div className="d-flex align-items-center">
+    <img src={IC_cuenta} alt="Cuenta" className="icono-header" />
+    <img src={IC_cart} alt="Carrito" className="icono-header ms-2" />
+  </div>
 </div>
 
-      <nav className="navbar navbar-expand-lg navbar-light custom-navbar">
+
+
+
+<nav className="navbar navbar-expand-lg navbar-light custom-navbar">
         <button
           className="navbar-toggler"
           type="button"
@@ -26,12 +50,13 @@ function Header() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-item nav-link active" href="#">Inicio</a>
+            <a className="nav-item nav-link inicio-link active" href="#">Inicio</a>
             <a className="nav-item nav-link" href="#">Estancias</a>
             <a className="nav-item nav-link" href="#">Sobre Nosotros</a>
-            <a className="nav-item nav-link" href="#">Contactanos</a>
+            <a className="nav-item nav-link" href="#">Contáctanos</a>
           </div>
         </div>
       </nav>
