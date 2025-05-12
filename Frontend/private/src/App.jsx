b7bb43c6bac601860af4d5ba793e1352c3835211
David Zepeda <20230231@ricaldone.edu.sv>
2025-05-12 07:45:22 -0600

    creé el private

---
+++ Frontend/private/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function App() {
+
+
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          {/* Aquí van las rutas de tus componentes */}
+          {/* <Route path="/ruta" element={<Componente />} /> */}
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+
+export default App
