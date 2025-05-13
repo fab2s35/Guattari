349f5e4b59823c1e1009ad883a55a28edf363a49
Paola <20230523@ricaldone.edu.sv>
2025-05-13 15:45:01 -0600

    Mini cambios

---
+++ Frontend/private/src/pages/MainPage/mainPage.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import './mainPage.css';
+
+function MainPage(){
+    const [mainPage, setMainPage] = useStat([
+        {
+            
+        }
+    ])
+
+}
