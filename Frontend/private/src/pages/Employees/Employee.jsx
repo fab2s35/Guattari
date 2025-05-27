753b5310c008e7d0fa7fa5bac5aaf5c145d49362
fab2s35 <sosorto72@gmail.com>
2025-05-27 00:17:59 -0600

    diseño employes

--- Frontend/private/src/pages/Employees/Employee.jsx
+++ Frontend/private/src/pages/Employees/Employee.jsx
@@ -295,7 +295,7 @@ const EmpleadosCRUD = () => {
         <h2 className="section-title">Listado de empleados ({empleadosFiltrados.length})</h2>
 
         {/* Barra de búsqueda y botón agregar */}
-        <div className="search-section">
+        <div className="search-section" >
           <div className="search-container">
             <div className="search-input-wrapper">
               <svg className="search-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -363,23 +363,18 @@ const EmpleadosCRUD = () => {
                           onClick={() => editarEmpleado(empleado)}
                           title="Editar empleado"
                         >
-                          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11 4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V13" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M18.5 2.50023C18.8978 2.1024 19.4374 1.87891 20 1.87891C20.5626 1.87891 21.1022 2.1024 21.5 2.50023C21.8978 2.89805 22.1213 3.43762 22.1213 4.00023C22.1213 4.56284 21.8978 5.1024 21.5 5.50023L12 15.0002L8 16.0002L9 12.0002L18.5 2.50023Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                          </svg>
+                          ✎
                         </button>
                         <button 
                           className="delete-btn"
                           onClick={() => eliminarEmpleado(empleado.id)}
                           title="Eliminar empleado"
                         >
-                          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3 6H5H21" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6M19 6V20C19 20.5304 18.7893 21.0391 18.4142 21.4142C18.0391 21.7893 17.5304 22 17 22H7C6.46957 22 5.96086 21.7893 5.58579 21.4142C5.21071 21.0391 5 20.5304 5 20V6H19Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                          </svg>
+                          🗑
                         </button>
                       </div>
                     </td>
+
                   </tr>
                 ))
               )}
